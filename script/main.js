2aad2113e4cecc72e6a7c96566c907188a6791c9
ahmed-ra7eem <66565015+ahmed-ra7eem@users.noreply.github.com>
2020-08-20 16:50:56 +0200

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,6 @@
+let share = document.getElementById('share'),
+    social = document.getElementById('social');
+
+share.addEventListener('click', function () {
+    social.classList.toggle('so-active');
+});
